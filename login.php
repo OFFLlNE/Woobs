b4c5366ca5e77fb1a98f6439c7989d649b0bb741
JHorm <johanneshorm@gmail.com>
2015-03-02 23:36:09 +0200

    establishing connection to database vol2

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "ouvcutnxpc.database.windows.net";
+$username = "csgorankingsdatabase";
+$password = "SammalHabe1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
