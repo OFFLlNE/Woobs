015f28f25a0953dd9551713b544153b9d1faa5a0
JHorm <johanneshorm@gmail.com>
2015-03-03 10:34:00 +0200

    testing

--- saladus/database_connection.php
+++ saladus/database_connection.php
@@ -5,6 +5,7 @@ $pwd = "SammalHabe1";
 $db = "csgorankingsdatabase";
 
 $conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+echo "$conn";
 
 if($conn === false){
     die(print_r(sqlsrv_errors()));
