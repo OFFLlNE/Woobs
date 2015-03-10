c9900e26bd99ef4483ed5260e027295b5c396fe4
keilu <keili.pedel@gmail.com>
2015-03-10 11:21:52 +0200

    Profiili uuendus

--- profile.php
+++ profile.php
@@ -25,22 +25,11 @@
 	<h1 id="heading">Profile</h1>
 	<div class="container-fluid">
 		<div class="row">
+			<div class="row">
 			<div class="col-xs-3 col-sm-3 col-md-3">
-				<h1 id="heading">Last Match</h1>
-				<div class="boxProfile">
-		    		<table style="width:100%">
-						<tr>
-							<td>blablabla</td>
-							<td>NUMBER</td>
-						</tr>
-						<tr>
-							<td>jne</td>
-							<td>NUMBER</td>
-						</tr>
-					</table>
-				</div>
+				<img><img src="img/profilepic.png" class="img-circle"/></img>
 			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6">
+			<div class="col-xs-9 col-sm-9 col-md-9">
 				<h1 id="heading">Overall Statistics</h1>
 				<div class="boxProfile">
 		    		<table style="width:100%">
@@ -67,11 +56,22 @@
 
 					</table>
 				</div>
+			</div>	
 			</div>
 			<div class="col-xs-3 col-sm-3 col-md-3">
-				<img>
-					<img src="img/profilepic.png" class="img-circle"/>
-				</img>
+				<h1 id="heading">Last Match</h1>
+				<div class="boxProfile">
+		    		<table style="width:100%">
+						<tr>
+							<td>blablabla</td>
+							<td>NUMBER</td>
+						</tr>
+						<tr>
+							<td>jne</td>
+							<td>NUMBER</td>
+						</tr>
+					</table>
+				</div>
 			</div>
 		</div>
 	</div>
