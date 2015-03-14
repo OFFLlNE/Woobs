7d82fdeaac308288d267c39a22ad395097318320
JHorm <johanneshorm@gmail.com>
2015-03-14 02:07:00 +0200

    get csgo data vol5

--- steamauth/csgoInfo.php
+++ steamauth/csgoInfo.php
@@ -3,7 +3,7 @@
     if (empty($_SESSION['steam_uptodate']) or $_SESSION['steam_uptodate'] == false or empty($_SESSION['steam_personaname'])) {
         $url = file_get_contents("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=".$steamauth['apikey']."&steamid=".$_SESSION['steamid']);
         $content = json_decode($url, true);
-        $_SESSION['total_kills'] = $content['playerstats']['stats'][0]['value'];
+        $_SESSION['total_kills'] = $content;
         $_SESSION['total_deaths'] = $content['playerstats']['stats'][1]['value'];
         $_SESSION['steam_uptodate'] = true;
     }
