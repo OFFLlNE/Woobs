94532dffa0577760ed480190360b3450041d0d88
JHorm <johanneshorm@gmail.com>
2015-03-14 18:39:10 +0200

    write to database test4

--- database/writeToUser.php
+++ database/writeToUser.php
@@ -1,12 +1,12 @@
 <?php
 include("database_connection.php");
 function writeToUser($steamID, $username, $avatar_small, $avatar_medium, $avatar_full, $profile_url) {
-	$query = "SELECT * FROM user WHERE steamID = '{intval($steamID)}'";
+	$query = "SELECT * FROM user WHERE steamID = '{$steamID}'";
 	$result = mysql_query($query)
 	 if(mysql_num_rows($query) == 0)
     {
 	 	$sql = "INSERT INTO user (steamID, username, avatar_small,avatar_medium, avatar_full, profile_url)
-		VALUES ('{intval($steamID)}', '{$username}', '{$avatar_small}', '{$avatar_medium}', '{$avatar_full}', '{$profile_url)}'";
+		VALUES ('{$steamID}', '{$username}', '{$avatar_small}', '{$avatar_medium}', '{$avatar_full}', '{$profile_url)}'";
 
 		if (mysqli_query($conn, $sql)) {
 		    echo "New record created successfully";
