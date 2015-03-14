17866559ee6373a5c759a3279d2fa5b900a2870f
JHorm <johanneshorm@gmail.com>
2015-03-14 18:51:40 +0200

    write to database test5

--- database/writeToUser.php
+++ database/writeToUser.php
@@ -3,9 +3,8 @@ include("database_connection.php");
 function writeToUser($steamID, $username, $avatar_small, $avatar_medium, $avatar_full, $profile_url) {
 	$query = "SELECT * FROM user WHERE steamID = '{$steamID}'";
 	$result = mysql_query($query)
-	 if(mysql_num_rows($query) == 0)
-    {
-	 	$sql = "INSERT INTO user (steamID, username, avatar_small,avatar_medium, avatar_full, profile_url)
+	 if(mysql_num_rows($query) == 0){
+	 	$sql = "INSERT INTO user (steamID, username, avatar_small ,avatar_medium, avatar_full, profile_url)
 		VALUES ('{$steamID}', '{$username}', '{$avatar_small}', '{$avatar_medium}', '{$avatar_full}', '{$profile_url)}'";
 
 		if (mysqli_query($conn, $sql)) {
@@ -14,8 +13,7 @@ function writeToUser($steamID, $username, $avatar_small, $avatar_medium, $avatar
 		    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 		}
 	    }
-    else
-    {
+    else{
     	$sql = "UPDATE user SET username='{$username}', avatar_small ='{$avatar_small}', avatar_medium='{$avatar_medium}', avatar_full='{$avatar_full}'
     	 WHERE steamID='{$steamID}'";
 		if (mysqli_query($conn, $sql)) {
