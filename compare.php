034202a59aa1b666ead32928deb8c2128c345cb3
JHorm <johanneshorm@gmail.com>
2015-03-16 23:09:32 +0200

    Major backend fixes with refactoring of whole project + live

--- compare.php
+++ compare.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Compare</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="main2.css">
-
-</head>
-<body>
 <?php $activePage = "compare.php"; ?>
 <div id="navigation">
 	<?php include('/menu.php'); ?>
--- live.php
+++ live.php
@@ -1,67 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Live</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="main2.css">
-
-</head>
-<body>
+<?php $activePage = "live.php"; ?>
 <div id="navigation">
 	<?php include('/menu.php'); ?>
 </div>
 <div class="middle">
-	<h1>
-		Live Streamerinos
+	<h1>Live Streamerinos</h1>
 		<div class="container-fluid">
-			<div class="col-xs-12s col-sm-12 col-md-6">
-				<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
-					<param name="allowFullScreen" value="true" />
-					<param name="allowScriptAccess" value="always" />
-					<param name="allowNetworking" value="all" />
-					<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
-					<param name="flashvars" value="channel=m0e_tv&auto_play=false&start_volume=25" />
-				</object>
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6">
+					<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
+						<param name="allowFullScreen" value="true" />
+						<param name="allowScriptAccess" value="always" />
+						<param name="allowNetworking" value="all" />
+						<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
+						<param name="flashvars" value="channel=m0e_tv&auto_play=false&start_volume=25" />
+					</object>
+				</div>
+
+				<div class="col-xs-6 col-sm-6 col-md-6">
+					<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
+						<param name="allowFullScreen" value="true" />
+						<param name="allowScriptAccess" value="always" />
+						<param name="allowNetworking" value="all" />
+						<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
+						<param name="flashvars" value="channel=esl_csgo&auto_play=false&start_volume=25" />
+					</object>
+				</div>
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-6">
-				<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
-					<param name="allowFullScreen" value="true" />
-					<param name="allowScriptAccess" value="always" />
-					<param name="allowNetworking" value="all" />
-					<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
-					<param name="flashvars" value="channel=esl_csgo&auto_play=false&start_volume=25" />
-				</object>
-			</div>
-			<div class="col-xs-12s col-sm-12 col-md-6">
-				<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
-					<param name="allowFullScreen" value="true" />
-					<param name="allowScriptAccess" value="always" />
-					<param name="allowNetworking" value="all" />
-					<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
-					<param name="flashvars" value="channel=Monstercat&auto_play=false&start_volume=25" />
-				</object>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-6">
-				<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
-					<param name="allowFullScreen" value="true" />
-					<param name="allowScriptAccess" value="always" />
-					<param name="allowNetworking" value="all" />
-					<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
-					<param name="flashvars" value="channel=esl_lol&auto_play=false&start_volume=25" />
-				</object>
+
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6">
+					<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
+						<param name="allowFullScreen" value="true" />
+						<param name="allowScriptAccess" value="always" />
+						<param name="allowNetworking" value="all" />
+						<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
+						<param name="flashvars" value="channel=Monstercat&auto_play=false&start_volume=25" />
+					</object>
+				</div>
+			
+				<div class="col-xs-6 col-sm-6 col-md-6">
+					<object type="application/x-shockwave-flash" height="240" width="360" data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" bgcolor="#000000">
+						<param name="allowFullScreen" value="true" />
+						<param name="allowScriptAccess" value="always" />
+						<param name="allowNetworking" value="all" />
+						<param name="movie" value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
+						<param name="flashvars" value="channel=esl_lol&auto_play=false&start_volume=25" />
+					</object>
+				</div>
 			</div>
 		</div>
-
-	</h1>
 		
 </div>
 
