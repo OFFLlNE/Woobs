c62a4b5042300fc20a7b03223c088339ae638fb1
keilu <keili.pedel@gmail.com>
2015-03-17 10:49:48 +0200

    textfieldid

--- compare.php
+++ compare.php
@@ -12,28 +12,36 @@
 	<h1 id="heading">Compare</h1>
 	<div class="container-fluid">
 		<div class="row">
-				<div class="col-xs-5 col-sm-5 col-md-5">
+				<div class="col-xs-3 col-sm-3 col-md-3">
 					<h1 id="heading">Player1</h1>
 					<img class="profilepic">
 			    		<img src="img/profilepic.png"/>
 					</img>
-					<p class="tekst">
-			    		<p>Mingi tekst</p>
-					</p>
+				</div>
+				<div class="col-xs-2 col-sm-2 col-md-2">
+					<form class="name">
+						Player1:
+						<br>
+							<input type="text" name="firstname">
+					</form>
 				</div>
 				<div class="col-xs-2 col-sm-2 col-md-2">
 					<button class="btn btn-default btn-lg">GO</button>
 				</div>
-				<div class="col-xs-5 col-sm-5 col-md-5">
+				<div class="col-xs-2 col-sm-2 col-md-2">
+					<form class="name">
+						Player2:
+						<br>
+							<input type="text" name="firstname">
+					</form>
+				</div>
+				<div class="col-xs-3 col-sm-3 col-md-3">
 					<h1 id="heading">
 						<h1>Player2</h1>
 					</h1>
 					<img class="profilepic">
 			    		<img src="img/profilepic.png"/>
 					</img>
-					<p class="tekst">
-			    		<p>Mingi tekst</p>
-					</p>
 				</div>
 			</div>
 		</div>
