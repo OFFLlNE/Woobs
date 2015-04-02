0846908a8d11d230265e648608d68e5fdd67b879
Lauri Välja <valja.lauri@gmail.com>
2015-04-02 20:29:03 +0300

    no message

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <?php
-echo "<p><ADDRESS> <A HREF=MAILTO:johanneshorm@hotmail.com>Woobs&copy</A> TÜ Informaatika </ADDRESS></p>";
+echo "<ADDRESS> <A HREF=MAILTO:johanneshorm@hotmail.com>Woobs&copy</A> TÜ Informaatika </ADDRESS>";
 ?>
