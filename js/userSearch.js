822e75e0e3c0e07f6aff03d07a953fb48e446fd5
JHorm <johanneshorm@gmail.com>
2015-04-04 23:32:34 +0300

    js fix may be 2

--- js/userSearch.js
+++ js/userSearch.js
@@ -2,13 +2,13 @@
 function showHint(str){
 
 if (str.length==0) { 
-    document.getElementById("userHint").innerHTML=" ";
+    document.getElementById("userHint").innerHTML="0";
     return;
 } else {
     var xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function() {
         if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-            document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+            document.getElementById("userHint").innerHTML=xmlhttp.responseText;
         }
     }
     xmlhttp.open("GET","/../database/predictUsers.php?q="+str,true);
