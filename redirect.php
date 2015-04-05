ea55a3c5b40d71d4bd8ebac6e7624bdfddea8cab
JHorm <johanneshorm@gmail.com>
2015-04-05 22:47:50 +0300

    redirect v1012

--- redirect.php
+++ redirect.php
@@ -4,14 +4,15 @@
 			if(isset($_SESSION['url'])){
 
 			   	if (strpos($_SESSION['url'],'signin.php') !== false) {
-			   		$url = "csgorankings.azurewebistes.net/profile.php"
+			   		$url = "csgorankings.azurewebistes.net/profile.php";
 				}
 				else{
 					$url = $_SESSION['url'];
 					}
 				}
-			else 
+			else{
 		   		$url = "csgorankings.azurewebistes.net/index.php"; // default page for 
+			}
 		   
 			header("Location: http://$url"); // perform correct redirect.
 			$_SESSION['redirect']=true;
