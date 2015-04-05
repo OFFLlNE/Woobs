8436aa851327e74befd2f9481ebe7a473102b0e1
keilu <keili.pedel@gmail.com>
2015-04-05 19:50:01 +0300

    no message

--- statistics.php
+++ statistics.php
@@ -37,7 +37,40 @@ include ('database/displayFunctions.php');
 		<div class="row">
 			<div id = "content_wrapper">
 				<div id="content">
-					<p>MAU</p>
+					<div class="col-xs-12 col-sm-12 col-md-12">
+				<h1 id="heading">Top 10 Money Wellspent</h1>
+				<div class="boxProfile">
+					<table style="width:100%">
+					<?php
+						MoneyWellSpent();
+					?>
+					</table>
+				</div>
+			<h1>Top 10 K/D Ratio</h1>
+				<div class="boxProfile">
+					<table style="width:100%">
+					<?php
+						KDRatio();
+					?>
+					</table>
+			</div>
+			<h1>Top 10 Nolifers</h1>
+				<div class="boxProfile">
+					<table style="width:100%">
+					<?php
+						nolifers();
+					?>
+					</table>
+			</div>
+			<h1>Top 10 Headhunters</h1>
+				<div class="boxProfile">
+					<table style="width:100%">
+					<?php
+						headhunters();
+					?>
+					</table>
+			</div>
+</div>
 				</div>
 			</div>
 		</div>
