56b21219e4a01798467e9e83972bebae538bd849
JHorm <johanneshorm@gmail.com>
2015-04-05 22:53:34 +0300

    I QUIT

--- redirect.php
+++ redirect.php
@@ -2,18 +2,10 @@
 	function redirect(){
 		if($_SESSION['redirect']==false){
 			if(isset($_SESSION['url'])){
-
-			   	if (strpos($_SESSION['url'],'signin') !== false) {
-			   		$url = "csgorankings.azurewebistes.net/profile.php";
-				}
-				else{
-					$url = $_SESSION['url'];
-					}
-				}
-			else{
+				$url = $_SESSION['url'];
+			else
 		   		$url = "csgorankings.azurewebistes.net/index.php"; // default page for 
-			}
-		   
+		
 			header("Location: http://$url"); // perform correct redirect.
 			$_SESSION['redirect']=true;
 		}
