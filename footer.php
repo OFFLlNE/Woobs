ae0a9e6032f863087e64d9a5d8cf5700794ee675
Lauri Välja <valja.lauri@gmail.com>
2015-04-14 11:02:33 +0300

    js footerinio

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 <div class="vertical-center">
 	<A HREF="MAILTO:johanneshorm@hotmail.com">Woobs&copy; </A> TÜ Informaatika
 </div>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<script type="text/javascript" property='javascript' src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/tab.js"></script>
+	<script src="js/userSearch.js"></script>
 </body>
 </html>
