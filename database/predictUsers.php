bc5023cc681d72a923088ead9c617d2dcef22667
JHorm <johanneshorm@gmail.com>
2015-04-21 01:19:46 +0300

    predict stuff5

--- database/predictUsers.php
+++ database/predictUsers.php
@@ -5,15 +5,15 @@ require_once("database_connection.php");
 
 $pdo = connect();
 $keyword = '%'.$_POST['keyword'].'%';
-$sql = "SELECT userName, SteamID FROM cs_user_view WHERE userName LIKE (:keyword) ORDER BY userName ASC LIMIT 0, 10";
+$sql = "SELECT userName, steamID FROM cs_user_view WHERE userName LIKE (:keyword) ORDER BY userName ASC LIMIT 0, 10";
 $query = $pdo->prepare($sql);
 $query->bindParam(':keyword', $keyword, PDO::PARAM_STR);
 $query->execute();
 $list = $query->fetchAll();
 foreach ($list as $rs) {
 	// put in bold the written text
-	$username = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['username']);
+	$username = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['userName']);
 	// add new option
-    echo '<li onclick="set_item(\''.str_replace("'", "\'", $rs['username']).'\')">'.$username.'</li>';
+    echo '<li onclick="set_item(\''.str_replace("'", "\'", $rs['userName']).'\')">'.$username.'</li>';
 }
 ?>
