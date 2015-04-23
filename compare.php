0a13af3d0097e45b59b6beae101e0cc9eda9f905
JHorm <johanneshorm@gmail.com>
2015-04-23 20:01:10 +0300

    1 thing to do

--- compare.php
+++ compare.php
@@ -7,7 +7,8 @@ $activePage = "compare.php";
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-3 col-sm-3 col-md-3">
-				<h1 >Player1</h1>
+			<div id="label_div1">Player1: </div>
+
 				<div class="profilepic">
 					<img src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
 				</div>
@@ -15,7 +16,7 @@ $activePage = "compare.php";
 			<div class="col-xs-3 col-sm-3 col-md-3">
 				<div class ="predict">
 					<form>
-						<div id="label_div1">Player1: </div>
+							<h1 >Player1</h1>
                				<div class="input_container">
                    				<input type="text" id="username_id" onkeyup="autocompleteUser()" >
                     			<ul id="username_list_id"></ul>
@@ -27,7 +28,7 @@ $activePage = "compare.php";
 			<div class="col-xs-3 col-sm-3 col-md-3">
 				<div class ="predict">
 					<form>
-						<div id="label_div2">Player2: </div>
+							<h1 >Player2</h1>
                				<div class="input_container">
                    				<input type="text" id="username_id2" onkeyup="autocompleteUser2()" >
                     			<ul id="username_list_id2"></ul>
@@ -37,7 +38,8 @@ $activePage = "compare.php";
 				</div>
 			</div>
 			<div class="col-xs-3 col-sm-3 col-md-3">
-				<h1 >Player2</h1>
+				<div id="label_div2">Player2: </div>
+
 				<div class="profilepic">
 					<img src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
 				</div>
