f047a290eed9a77936785da177cf30877039d66f
JHorm <johanneshorm@gmail.com>
2015-04-23 19:37:05 +0300

    works, now for 2nd

--- compare.php
+++ compare.php
@@ -12,7 +12,7 @@ $activePage = "compare.php";
 					<img src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
 				</div>
 			</div>
-			<div class="col-xs-2 col-sm-2 col-md-2">
+			<div class="col-xs-3 col-sm-3 col-md-3">
 				<div class ="predict">
 					<form>
 						<div class="label_div">Player1: </div>
@@ -24,28 +24,34 @@ $activePage = "compare.php";
 
 				</div>
 			</div>
-			<div class="col-xs-2 col-sm-2 col-md-2">
-				<button class="btn btn-default btn-lg">GO</button>
-
-			</div>
-			<div class="col-xs-2 col-sm-2 col-md-2">
-					Player2:
-
-			<div class="userNames">
-			  <label for="users"></label>
-			  <input id="users">
-			</div>
 			<div class="col-xs-3 col-sm-3 col-md-3">
-				<h1>Player2</h1>
+				<h1 >Player2</h1>
 				<div class="profilepic">
 					<img src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
 				</div>
 			</div>
-		</div>
+			<div class="col-xs-3 col-sm-3 col-md-3">
+				<div class ="predict">
+					<form>
+						<div class="label_div">Player2: </div>
+               				<div class="input_container2">
+                   				<input type="text" id="username_id2" onkeyup="autocompleteUser2()" >
+                    			<ul id="username_list_id2"></ul>
+                			</div>
+					</form>
+
+				</div>
+			</div>
 		<div class="row">
-			<div class="col-xs-2 col-sm-2 col-md-2">
+			<div class="col-xs-6 col-sm-6 col-md-6">
 
 				<div id="player1stat">
+
+				</div>
+			</div>
+			<div class="col-xs-6 col-sm-6 col-md-6">
+
+				<div id="player2stat">
 
 				</div>
 			</div>
---
+++ js/userSearch2.js
@@ -0,0 +1,58 @@
+var autocompleteResults = [];
+var selected;
+
+function autocompleteUser2() {
+    var min_length = 0; // min caracters to display the autocomplete
+    var keyword = $('#username_id2').val();
+    if (keyword.length >= min_length) {
+        $.ajax({
+            url: '../database/predictUsers.php',
+            type: 'POST',
+            data: {keyword:keyword},
+            success: function(data){
+
+                var data = JSON.parse(data);
+
+                autocompleteResults = data;
+
+                $('#username_list_id2').show();
+                $('#username_list_id2').html('');
+
+                for(var i = 0; i < data.length; i++) {
+                    $('#username_list_id2').append('<li onclick="markSelected(' + i + ')">' + data[i].userName + '</li>');
+                }
+            }
+        });
+    } else {
+        $('#username_list_id2').hide();
+    }
+}
+
+function markSelected (index) {
+    selected = autocompleteResults[index];
+    var name = selected.userName;
+    var steamID = selected.steamID;
+    console.log(steamID);
+    set_item(name);
+    $.ajax({
+            url: '../database/compareUser.php',
+            type: 'POST',
+            data: {steamID:steamID},
+            success: function(data){
+
+                var data = JSON.parse(data);
+                console.log(data[0]);
+                $('#player2stat').html(data[0]);
+
+            }
+        });
+
+}
+
+// set_item : this function will be executed when we select an item
+function set_item(item) {
+    // change input value
+    $('#username_id2').val(item);
+    // hide proposition list
+    $('#username_list_id2').hide();
+}
