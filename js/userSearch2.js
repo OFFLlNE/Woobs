739efb837ed02e335a58aac715765d893f0b871e
JHorm <johanneshorm@gmail.com>
2015-04-24 03:05:54 +0300

    stupid me

--- js/userSearch2.js
+++ js/userSearch2.js
@@ -34,7 +34,7 @@ function markSelected2 (index) {
     var steamID = selected.steamID;
     console.log(steamID);
     set_item2(name);
-    $('#label_div2').html(data[0]);
+    $('#label_div2').html(name);
     $.ajax({
             url: '../database/compareUser.php',
             type: 'POST',
