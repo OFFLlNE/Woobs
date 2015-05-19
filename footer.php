bc06f4a5055cc2cea3cc2efd78fe807243e41d8d
keilu <keili.pedel@gmail.com>
2015-05-19 12:28:47 +0300

    no message

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 	<script type="text/javascript" property='javascript' src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 	<script src="js/tab2.js"></script>
-	<script type="text/javascript" src="js/renew.js"></script>
+	<!--<script type="text/javascript" src="js/renew.js"></script>-->
 	<script src="js/userSearch.js"></script>
 	<script src="js/userSearch2.js"></script>
 </body>
