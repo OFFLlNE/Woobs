e8454181384c225206b7dedc164b005d5ad4b718
JHorm <johanneshorm@gmail.com>
2015-05-24 00:25:36 +0300

    compare is responsive

--- compare.php
+++ compare.php
@@ -6,46 +6,55 @@ $activePage = "compare.php";
 	<h1>Compare</h1>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-xs-3 col-sm-3 col-md-3">
-				<div id="label_div1"></div>
+			<div class="col-xs-6 col-sm-6 col-md-6">
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 col-md-6">
+						<div id="label_div1"></div>
 
-				<div class="profilepic">
-					<div id="profile1">
-						<img class="img-responsive pull-right" src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
+						<div class="profilepic">
+							<div id="profile1">
+								<img class="img-responsive pull-right" src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-6">
+						<div class ="predict">
+							<form class="pull-right">
+									<h1 class="pull-right">Player1</h1>
+		               				<div class="input_container">
+		                   				<input type="text" id="username_id" onkeyup="autocompleteUser()" >
+		                    			<ul id="username_list_id"></ul>
+		                			</div>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-xs-3 col-sm-3 col-md-3">
-				<div class ="predict">
-					<form class="pull-left">
-							<h1 class="pull-right">Player1</h1>
-               				<div class="input_container">
-                   				<input type="text" id="username_id" onkeyup="autocompleteUser()" >
-                    			<ul id="username_list_id"></ul>
-                			</div>
-					</form>
+			<div class="col-xs-6 col-sm-6 col-md-6">
+				<div class="row">
+					<div class="col-xs-12 col-md-6 col-md-push-6">
 
-				</div>
-			</div>
-			<div class="col-xs-3 col-sm-3 col-md-3">
-				<div class ="predict">
-					<form class="pull-right">
-							<h1 class="pull-left">Player2</h1>
-               				<div class="input_container">
-                   				<input type="text" id="username_id2" onkeyup="autocompleteUser2()" >
-                    			<ul id="username_list_id2"></ul>
-                			</div>
-					</form>
+						<div id="label_div2"></div>
 
-				</div>
-			</div>
-			<div class="col-xs-3 col-sm-3 col-md-3">
-				<div id="label_div2"></div>
-
-				<div class="profilepic">
-					<div id="profile2">
-						<img class="img-responsive pull-left" src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
+						<div class="profilepic">
+							<div id="profile2">
+								<img class="img-responsive" src="img/profilepic.png" alt="Relevant textual alternative to the image"/>
+							</div>
+						</div>
 					</div>
+					<div class="col-xs-12 col-md-6 col-md-pull-6">
+						<div class ="predict">
+							<form class="pull-left">
+									<h1 class="pull-left">Player2</h1>
+		               				<div class="input_container">
+		                   				<input type="text" id="username_id2" onkeyup="autocompleteUser2()" >
+		                    			<ul id="username_list_id2"></ul>
+		                			</div>
+							</form>
+
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
