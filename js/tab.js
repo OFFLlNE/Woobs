c2b28ec7e60a464598fb2ed432b4e407a6266670
Lauri Välja <valja.lauri@gmail.com>
2015-05-24 18:50:23 +0300

    katsetused no.1

--- js/tab.js
+++ js/tab.js
@@ -1,21 +1,10 @@
-$(function pageload(hash){
-	$("#tabs a").click(function(){
-					var page = this.hash.substring(1);
-    					$.get(page+".php", function(gotHtml){
-						$("#content").html(gotHtml);
-					});
-				});
-			});
 
 $(document).ready(function(){
-    // Initialize history plugin.
-    $.history.init(pageload);
 
     $("a").on("click",function(){
         if ($(this).attr("href").charAt(0) == '#'){
             var hash = $(this).attr("href");
             hash = hash.replace(/^.*#/, '');
-            $.history.load(hash);
         }
     })
 });
