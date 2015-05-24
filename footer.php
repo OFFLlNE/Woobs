796b73f1f88a87aefb5c6c7e1515502a4fc5977c
Lauri Välja <valja.lauri@gmail.com>
2015-05-24 18:53:11 +0300

    katsetused no.2

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div>
 	<script type="text/javascript" property='javascript' src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-	<script src="js/tab2.js"></script>
+	<script src="js/tab.js"></script>
 	<!--<script type="text/javascript" src="js/renew.js"></script>-->
 	<script src="js/userSearch.js"></script>
 	<script src="js/userSearch2.js"></script>
