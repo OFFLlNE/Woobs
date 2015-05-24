229e61bb2500c3935d6f70324234953d8617fa81
Lauri Välja <valja.lauri@gmail.com>
2015-05-24 19:14:52 +0300

    katsetused no.9

--- js/tab.js
+++ js/tab.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     $("a").on("click",function(){
         if ($(this).attr("href").charAt(0) == '#'){
             var hash = $(this).attr("href");
+            hash = hash.replace(/^.*#/, '');
         }
     })
 });
