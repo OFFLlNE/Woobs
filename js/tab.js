8a1710a01b1fdd84fda13cd049ada5784ce14749
Lauri Välja <valja.lauri@gmail.com>
2015-05-24 19:25:40 +0300

    katsetused no.9

--- js/tab.js
+++ js/tab.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-    $("a").on("click",function(){
+    $("a").live("click",function(){
         if ($(this).attr("href").charAt(0) == '#'){
             var hash = $(this).attr("href");
             hash = hash.replace(/^.*#/, '');
