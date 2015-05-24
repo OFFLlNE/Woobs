e75a8cb60bc72f01f11a7b280f3a2c4103c2ce3e
Lauri Välja <valja.lauri@gmail.com>
2015-05-25 00:13:32 +0300

    katsetused no.50

--- katse.php
+++ katse.php
@@ -23,132 +23,47 @@ include ('database/displayFunctions.php');
 							<th>Kill/Death ratio</th>
 							</tr><tr>
 							<?php
-						KDRatio();
+								KDRatio();
 							?>
 			</table>
 
 			<table style="width:100%">
-					<tr>
 							<th>Username</th>
 							<th>Hours played last 2 weeks</th>
 							</tr><tr>
-							<td>MoMentous</td>
-							<td>50.27</td>
-							</tr><tr>
-							<td>root</td>
-							<td>49.65</td>
-							</tr><tr>
-							<td>Little Ponies</td>
-							<td>41.18</td>
-							</tr><tr>
-							<td>THIS. IS. MELL!</td>
-							<td>20.57</td>
-							</tr><tr>
-							<td>Priidrik</td>
-							<td>14.15</td>
-							</tr><tr>
-							<td>Josh</td>
-							<td>11.87</td>
-							</tr><tr>
-							<td>HotPapa</td>
-							<td>8.78</td>
-							</tr><tr>
-							<td>HotPapa</td>
-							<td>6.12</td>
-							</tr><tr>
-							<td>THIS. lS. MELL!</td>
-							<td>3.93</td>
-							</tr><tr>
-							<td>Mihkel</td>
-							<td>3.83</td>
-							</tr><tr>
-							<td>Get eRekt</td>
-							<td>3.35</td>
-							</tr>					</table>
+							<?php
+								nolifers();
+							?>	
+			</table>
 
 		</div>
 		<div id="cs2" style="display: none;">
 			<table class ="cs2" style="width:100%">
-						<tr>
 							<th>Username</th>
 							<th>Overall kills</th>
 							</tr><tr>
-							<td>HotPapa</td>
-							<td>52168</td>
-							</tr><tr>
-							<td>HotPapa</td>
-							<td>8333</td>
-							</tr><tr>
-							<td>Little Ponies</td>
-							<td>6954</td>
-							</tr><tr>
-							<td>root</td>
-							<td>3648</td>
-							</tr><tr>
-							<td>THIS. lS. MELL!</td>
-							<td>3206</td>
-							</tr><tr>
-							<td>Josh</td>
-							<td>1913</td>
-							</tr><tr>
-							<td>Get eRekt</td>
-							<td>1759</td>
-							</tr><tr>
-							<td>MoMentous</td>
-							<td>1397</td>
-							</tr><tr>
-							<td>Mihkel</td>
-							<td>1166</td>
-							</tr><tr>
-							<td>Priidrik</td>
-							<td>1085</td>
-							</tr><tr>
-							<td>THIS. IS. MELL!</td>
-							<td>871</td>
-							</tr>
-							
-						
+							<?php
+								totalKills();
+							?>
 				</table>
+
+				<table style="width:100%">
+							<th>Username</th>
+							<th>Headshot kills</th>
+							</tr><tr>
+							<?php
+								totalHeadShotKills();
+							?>	
+			</table>
 		</div>
 		<div id="cs3" style="display: none;">
 			<table class ="cs3" style="width:100%">
-						<tr>
 							<th>Username</th>
 							<th>Accuracy percentage</th>
 							</tr><tr>
-							<td>HotPapa</td>
-							<td>35.93</td>
-							</tr><tr>
-							<td>root</td>
-							<td>26.28</td>
-							</tr><tr>
-							<td>HotPapa</td>
-							<td>24.09</td>
-							</tr><tr>
-							<td>Mihkel</td>
-							<td>21.81</td>
-							</tr><tr>
-							<td>Josh</td>
-							<td>20.61</td>
-							</tr><tr>
-							<td>Little Ponies</td>
-							<td>20.01</td>
-							</tr><tr>
-							<td>MoMentous</td>
-							<td>19.27</td>
-							</tr><tr>
-							<td>Priidrik</td>
-							<td>19.02</td>
-							</tr><tr>
-							<td>Get eRekt</td>
-							<td>17.89</td>
-							</tr><tr>
-							<td>THIS. lS. MELL!</td>
-							<td>17.36</td>
-							</tr><tr>
-							<td>THIS. IS. MELL!</td>
-							<td>16.17</td>
-							</tr>
+							<?php
+								rifle_acc();
+							?>
 							
 						
 				</table>
