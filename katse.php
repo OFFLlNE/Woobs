eb8d8de2fc0ed0c45659280dfc0db237e3dcfe1d
Lauri Välja <valja.lauri@gmail.com>
2015-05-24 22:19:07 +0300

    katsetused no.20

--- katse.php
+++ katse.php
@@ -16,17 +16,79 @@ include ('database/displayFunctions.php');
                        
 			<table class="cs1" id ="t01">
 				<h1 id="heading">Top 10 Money Wellspent</h1>
-					<?php
-						MoneyWellSpent();
-					?>
+					<tr>
+							<td>HotPapa</td>
+							<td>2.51</td>
+							</tr><tr>
+							<td>HotPapa</td>
+							<td>1.43</td>
+							</tr><tr>
+							<td>root</td>
+							<td>1.28</td>
+							</tr><tr>
+							<td>Josh</td>
+							<td>0.90</td>
+							</tr><tr>
+							<td>THIS. IS. MELL!</td>
+							<td>0.75</td>
+							</tr><tr>
+							<td>MoMentous</td>
+							<td>0.60</td>
+							</tr><tr>
+							<td>THIS. lS. MELL!</td>
+							<td>0.59</td>
+							</tr><tr>
+							<td>Little Ponies</td>
+							<td>0.57</td>
+							</tr><tr>
+							<td>Mihkel</td>
+							<td>0.50</td>
+							</tr><tr>
+							<td>Priidrik</td>
+							<td>0.49</td>
+							</tr><tr>
+							<td>Get eRekt</td>
+							<td>0.48</td>
+							</tr>
 			</table>
 		</div>
 		<div id="cs2" style="display: none;">
 			<table class ="cs2" id="t02">
 					<h1 id="heading">Top 10 Total Kills Headshot</h1>
-						<?php
-						totalHeadShotKills();
-					?>
+						<tr>
+							<td>HotPapa</td>
+							<td>52168</td>
+							</tr><tr>
+							<td>HotPapa</td>
+							<td>8333</td>
+							</tr><tr>
+							<td>Little Ponies</td>
+							<td>6954</td>
+							</tr><tr>
+							<td>root</td>
+							<td>3648</td>
+							</tr><tr>
+							<td>THIS. lS. MELL!</td>
+							<td>3206</td>
+							</tr><tr>
+							<td>Josh</td>
+							<td>1913</td>
+							</tr><tr>
+							<td>Get eRekt</td>
+							<td>1759</td>
+							</tr><tr>
+							<td>MoMentous</td>
+							<td>1397</td>
+							</tr><tr>
+							<td>Mihkel</td>
+							<td>1166</td>
+							</tr><tr>
+							<td>Priidrik</td>
+							<td>1085</td>
+							</tr><tr>
+							<td>THIS. IS. MELL!</td>
+							<td>871</td>
+							</tr>
 							
 						
 				</table>
@@ -34,9 +96,40 @@ include ('database/displayFunctions.php');
 		<div id="cs3" style="display: none;">
 			<table class ="cs3" id="t03">
 					<h1 id="heading">Top 10 Pistol Accuracy</h1>
-						<?php
-						pistol_acc();
-					?>
+						<tr>
+							<td>HotPapa</td>
+							<td>35.93</td>
+							</tr><tr>
+							<td>root</td>
+							<td>26.28</td>
+							</tr><tr>
+							<td>HotPapa</td>
+							<td>24.09</td>
+							</tr><tr>
+							<td>Mihkel</td>
+							<td>21.81</td>
+							</tr><tr>
+							<td>Josh</td>
+							<td>20.61</td>
+							</tr><tr>
+							<td>Little Ponies</td>
+							<td>20.01</td>
+							</tr><tr>
+							<td>MoMentous</td>
+							<td>19.27</td>
+							</tr><tr>
+							<td>Priidrik</td>
+							<td>19.02</td>
+							</tr><tr>
+							<td>Get eRekt</td>
+							<td>17.89</td>
+							</tr><tr>
+							<td>THIS. lS. MELL!</td>
+							<td>17.36</td>
+							</tr><tr>
+							<td>THIS. IS. MELL!</td>
+							<td>16.17</td>
+							</tr>
 							
 						
 				</table>
