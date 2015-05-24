bc2036cf4d80a6d4c3fba1d48353a9e40890b544
JHorm <johanneshorm@gmail.com>
2015-05-25 00:47:52 +0300

    bookmarks and history

--- js/tab.js
+++ js/tab.js
@@ -1,21 +1,76 @@
-$(function pageload(hash){
-	$("#tabs a").click(function(){
-					var page = this.hash.substring(1);
-    					$.get(page+".php", function(gotHtml){
-						$("#content").html(gotHtml);
-					});
-				});
-			});
+$(document).ready(function() {
+    function addingListener() {
+        window.addEventListener("popstate", function(e) {
+            if (e.state == null) {
+                show1();
+            } else if (e.state.view == "cs1") {
+                show1();
+            } else if (e.state.view == "cs2") {
+                show2();
+            } else if (e.state.view == "cs3") {
+                show3();
+            }
+        });
+    };
 
-$(document).ready(function(){
-    // Initialize history plugin.
-    $.history.init(pageload);
+    function hideAll() {
+        $("#cs1").hide();
+        $("#cs2").hide();
+        $("#cs3").hide();
+    }
 
-    $("a").on("click",function(){
-        if ($(this).attr("href").charAt(0) == '#'){
-            var hash = $(this).attr("href");
-            hash = hash.replace(/^.*#/, '');
-            $.history.load(hash);
+    function show1() {
+        $("#cs1").show();
+        $("#cs2").hide();
+        $("#cs3").hide();
+    }
+
+    function show2() {
+        $("#cs1").hide();
+        $("#cs2").show();
+        $("#cs3").hide();
+    }
+
+    function show3() {
+        $("#cs1").hide();
+        $("#cs2").hide();
+        $("#cs3").show();
+    }
+
+
+    var url = window.location.href;
+    console.log(url.search("#"));
+    if (url.search("#") == -1) {
+        show1();
+    } else {
+        var a = url.substr(url.indexOf("#") + 1);
+        if (a == "Overall") {
+           show1();
+        } else if (a == "Bestkillers") {
+            show2();
+        } else if (a == "Accuracy") {
+            show3();
+        } else {
+            hideAll();
         }
-    })
+    }
+
+    $("#CSS1").click(function(){
+        show1();
+        window.history.pushState({'view': 'cs1'}, "Overall", "/statistics.php#Overall");
+        addingListener();
+        return false;
+    });
+    $("#CSS2").click(function(){
+        show2();
+        window.history.pushState({'view': 'cs2'}, "Bestkillers", "/statistics.php#Bestkillers");
+        addingListener();
+        return false;
+    });
+    $("#CSS3").click(function(){
+        show3();
+        window.history.pushState({'view': 'cs3'}, "Accuracy", "/statistics.php#Accuracy");
+        addingListener();
+        return false;
+    });
 });
--- statistics.php
+++ statistics.php
@@ -6,50 +6,102 @@ include ('database/displayFunctions.php');
 <div class="middle">
 	<h1>Statistics</h1>
 	<div class="container-fluid">
+		<ul id="tabs">
+			<li><a id="CSS1">Overall</a></li>
+			<li><a id="CSS2">Bestkillers</a></li>
+			<li><a id="CSS3">Accuracy</a></li>
+		</ul>
 		<div class="row">
-			<ul id="tabs">
-				<li><a id="CS1" href="#content1">Overall</a></li>
-				<li><a id="CS2" href="#content2">Best Killers</a></li>
-				<li><a id="CS3" href="#content3">Accuracy</a></li>
-			</ul>
-		</div>
-		<div class="row">
-			<div class = "content_wrapper">
-				<div id = "content">
+			<div class="content_wrapper">
+				<div class="content">
 					<div class="col-xs-12 col-sm-12 col-md-12">
-				<h1>Top 10 Money Wellspent</h1>
-				<div class="boxProfile">
-					<table style="width:100%">
-					<?php
-						MoneyWellSpent();
-					?>
-					</table>
-				</div>
-			<h1>Top 10 K/D Ratio</h1>
-				<div class="boxProfile">
-					<table style="width:100%">
-					<?php
-						KDRatio();
-					?>
-					</table>
-			</div>
-			<h1>Top 10 Nolifers</h1>
-				<div class="boxProfile">
-					<table style="width:100%">
-					<?php
-						nolifers();
-					?>
-					</table>
-			</div>
-			<h1>Top 10 Headhunters</h1>
-				<div class="boxProfile">
-					<table style="width:100%">
-					<?php
-						headhunters();
-					?>
-					</table>
-			</div>
-</div>
+						<div id="cs1">
+							<h1>Top 10 Money Wellspent</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									MoneyWellSpent();
+								?>
+								</table>
+							</div>
+							<h1>Top 10 K/D Ratio</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									KDRatio();
+								?>
+								</table>
+							</div>
+							<h1>Top 10 Nolifers</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									nolifers();
+								?>
+								</table>
+							</div>
+							<h1>Top 10 Headhunters</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									headhunters();
+								?>
+								</table>
+							</div>
+						</div>
+						<div id="cs2" style="display: none;">
+							<h1 id="heading">Top 10 Total Kills Headshot</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									totalHeadShotKills();
+								?>
+								</table>
+								</div>
+							<h1>Top 10 Total Kills</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									totalKills();
+								?>
+								</table>
+							</div>
+							<h1>Top 10 AWP Kills</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									total_kills_awp();
+								?>
+								</table>
+							</div>
+						</div>
+						<div id="cs3" style="display: none;">
+							<h1 id="heading">Top 10 Pistol Accuracy</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									pistol_acc();
+								?>
+								</table>
+							</div>
+							<h1>Top 10 Rifle Accuracy</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									rifle_acc();
+								?>
+								</table>
+							</div>
+							<h1>Top 10 Sniper Accuracy</h1>
+							<div class="boxProfile">
+								<table style="width:100%">
+								<?php
+									sniper_acc();
+								?>
+								</table>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
