7bedb62a5e849bd5b2e876a731c6d2272b84111b
Lauri Välja <valja.lauri@gmail.com>
2015-05-24 23:17:27 +0300

    katsetused no.34

--- footer.php
+++ footer.php
@@ -8,6 +8,5 @@
 	<script type="text/javascript" src="js/userSearch2.js"></script>
 	<script type="text/javascript" src="js/changePage.js"></script>
 	<script type="text/javascript" src="js/tab2.js"></script>
-	<script type="text/javascript" src="js/benal.js"></script>
 </body>
 </html>
